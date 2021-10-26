a16afd52a0fbeffddd4ee70805733d8dfa077243
Kamaljit <kamaljitbhuller@gmail.com>
2021-10-26 15:48:04 -0700

    initial setup for the project

---
+++ controller/reminder_controller.js
@@ -0,0 +1,52 @@
+let database = require("../database");
+
+let remindersController = {
+  list: (req, res) => {
+    res.render("reminder/index", { reminders: database.cindy.reminders });
+  },
+
+  new: (req, res) => {
+    res.render("reminder/create");
+  },
+
+  listOne: (req, res) => {
+    let reminderToFind = req.params.id;
+    let searchResult = database.cindy.reminders.find(function (reminder) {
+      return reminder.id == reminderToFind;
+    });
+    if (searchResult != undefined) {
+      res.render("reminder/single-reminder", { reminderItem: searchResult });
+    } else {
+      res.render("reminder/index", { reminders: database.cindy.reminders });
+    }
+  },
+
+  create: (req, res) => {
+    let reminder = {
+      id: database.cindy.reminders.length + 1,
+      title: req.body.title,
+      description: req.body.description,
+      completed: false,
+    };
+    database.cindy.reminders.push(reminder);
+    res.redirect("/reminders");
+  },
+
+  edit: (req, res) => {
+    let reminderToFind = req.params.id;
+    let searchResult = database.cindy.reminders.find(function (reminder) {
+      return reminder.id == reminderToFind;
+    });
+    res.render("reminder/edit", { reminderItem: searchResult });
+  },
+
+  update: (req, res) => {
+    // implement this code
+  },
+
+  delete: (req, res) => {
+    // Implement this code
+  },
+};
+
+module.exports = remindersController;
---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const ejsLayouts = require("express-ejs-layouts");
+const reminderController = require("./controller/reminder_controller");
+const authController = require("./controller/auth_controller");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(ejsLayouts);
+
+app.set("view engine", "ejs");
+
+// Routes start here
+
+app.get("/reminders", reminderController.list);
+
+app.get("/reminder/new", reminderController.new);
+
+app.get("/reminder/:id", reminderController.listOne);
+
+app.get("/reminder/:id/edit", reminderController.edit);
+
+app.post("/reminder/", reminderController.create);
+
+// Implement this yourself
+app.post("/reminder/update/:id", reminderController.update);
+
+// Implement this yourself
+app.post("/reminder/delete/:id", reminderController.delete);
+
+// Fix this to work with passport! The registration does not need to work, you can use the fake database for this.
+app.get("/register", authController.register);
+app.get("/login", authController.login);
+app.post("/register", authController.registerSubmit);
+app.post("/login", authController.loginSubmit);
+
+app.listen(3001, function () {
+  console.log(
+    "Server running. Visit: localhost:3001/reminders in your browser 🚀"
+  );
+});
