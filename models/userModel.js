e43689ea49dec70895697c7115599f5745b6271d
Martin Lee <martinlee5159@gmail.com>
2021-11-09 15:42:31 -0800

    add roles to userModel

--- models/userModel.js
+++ models/userModel.js
@@ -4,18 +4,28 @@ const database = [
     name: "Jimmy Smith",
     email: "jimmy123@gmail.com",
     password: "jimmy123!",
+    role: "user"
   },
   {
     id: 2,
     name: "Johnny Doe",
     email: "johnny123@gmail.com",
     password: "johnny123!",
+    role: "user"
   },
   {
     id: 3,
     name: "Jonathan Chen",
     email: "jonathan123@gmail.com",
     password: "jonathan123!",
+    role: "user"
+  },
+  {
+    id: 4,
+    name: "Martin Lee",
+    email: "martin123@gmail.com",
+    password: "martin123!",
+    role: "admin"
   },
 ];
 
