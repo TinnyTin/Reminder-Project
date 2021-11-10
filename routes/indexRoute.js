0bbed661808f6169b81bf7ce4e411cec6b4860a5
Judy Chen <shiyichen95@gmail.com>
2021-11-09 21:10:55 -0800

    add some code to indexroute

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const { ensureAuthenticated, isAdmin } = require("../middleware/checkAuth");
+require('dotenv').config()
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
   res.send("welcome");
+  const clientId = process.env.CLIENT_ID; 
+  const query = "people"
+  const url = "https://api.unsplash.com/search/photos?client_id=${clientId}&query=${query}";
+  const data = await fetch(url);
+  const jsonData = await data.json()
+    const imageFromUnsplash = jsonData.results;
+    console.log(imageFromUnsplash)
 });
 
 router.get("/dashboard", ensureAuthenticated, (req, res) => {
